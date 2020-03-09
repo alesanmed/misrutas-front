7f62a27c161aeb7cd159a52dbe19e133e190b700
Ale Sánchez <ale@rebellionpay.com>
2020-03-09 13:01:41 +0100

    Added routes and leaflet

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app-bar
+      app
+      color="primary"
+      dark
+      flat
+    >
+      <div class="d-flex align-center">
+        <router-link to="/" class="display-2" tag="button">Mis Rutas</router-link>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href=""
+        target="_blank"
+        icon
+      >
+        <router-link to="/about" tag="span"><v-icon>person</v-icon></router-link>
+      </v-btn>
+    </v-app-bar>
+</template>
